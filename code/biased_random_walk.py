2434f909a53685114f36c8b2d0460cb834c738c2
Bijay Gaudel <gaudelbijay10@gmail.com>
2019-12-19 15:01:11 +0800

    calculate transitrion_probability

--- code/biased_random_walk.py
+++ code/biased_random_walk.py
@@ -11,7 +11,18 @@ class BiasedRandomWalk:
     def preprocess_transition_probability(self):
         G = self.G
         alias_node = {}
+        for node in G.nodes():
+            unnormalized_probability = [G[node][nbrs].get('weight',1.0) for nbrs in G.neighbors(node)]
+            norm_const = sum(unnormalized_probability)
+            normalized_probability = [float(up)/norm_const for up in unnormalized_probability]
+            alias_node[node] = create_alias_table(normalized_probability)
+
+        alias_edge = {}
+        for edge in G.edges():
+            alias_edge[edge] = get_alias_edge(edge[0],edge[1])
         
+        self.alias_node = alias_node
+        self.alias_edge = alias_edge
 
 
 
