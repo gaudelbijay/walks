c9eac7dbd96614de2fa1d9a0d95f936d663a4aa4
Bijay Gaudel <gaudelbijay10@gmail.com>
2019-12-19 17:11:19 +0800

    alias edge

--- code/biased_random_walk.py
+++ code/biased_random_walk.py
@@ -19,16 +19,40 @@ class BiasedRandomWalk:
 
         alias_edge = {}
         for edge in G.edges():
-            alias_edge[edge] = get_alias_edge(edge[0],edge[1])
+            print(edge)
+            alias_edge[edge] = self.get_alias_edge(edge[0],edge[1])
         
         self.alias_node = alias_node
         self.alias_edge = alias_edge
 
+    def get_alias_edge(self,u,v):
+        G = self.G 
+        p = self.p 
+        q = self.q 
 
+        unnormalized_probability = []
+        for x in G.neighbors(v):
+            weight = G[v][x].get('weight',1.0)
+            if x == u:
+                unnormalized_probability.append(weight/p)
+            elif G.has_edge(x,u):
+                unnormalized_probability.append(weight)
+            else:
+                unnormalized_probability.append(weight/q)
+            norm_const = sum(unnormalized_probability)
+            normalized_probability = [float(up)/norm_const for up in unnormalized_probability]
+
+            return create_alias_table(normalized_probability)
+            
+    def create_alias_table(area_ratio):
+        pass
 
 
 def main():
     graph = nx.read_edgelist('../data/Wiki_edgelist.txt',create_using=nx.DiGraph(),nodetype=None,data=[('weight',int)])
+    brw = BiasedRandomWalk(graph)
+    brw.preprocess_transition_probability()
+
 
 if __name__ == '__main__':
     main()
